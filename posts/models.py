5e32ccec61a4ae8c622336dc4bf543820d1f0c8b
RShveda <rshveda@gmail.com>
2020-08-21 11:33:39 +0300

    Added embeded PostCreate form to a Group Page

--- posts/models.py
+++ posts/models.py
@@ -29,3 +29,6 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('posts:post_detail', kwargs={'pk':self.pk})
+
+    class Meta:
+        ordering = ['-created_at']
