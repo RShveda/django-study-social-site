d9e924887d6ed2405741777ddab8db29d08f39e3
RShveda <rshveda@gmail.com>
2020-09-11 15:26:27 +0300

    added UI and FrontEnd functionality for post voting

--- posts/templatetags/post_extras.py
+++ posts/templatetags/post_extras.py
@@ -1,7 +1,18 @@
 from django import template
+from posts.models import PostVotes
 
 register = template.Library()
 
 @register.filter
 def subtract(value, arg):
     return value - arg
+
+@register.filter
+def vote_value(value, arg):
+    try:
+        post_vote = PostVotes.objects.get(post=value, voter=arg)
+        # print("yor vote is " + str(post_vote.vote))
+        return post_vote.vote
+    except:
+        # print("error occured while trying to access user postvote")
+        pass
